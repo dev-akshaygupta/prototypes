ba89364914d701713724d573241145f3c715567f
Akshay Gupta <33322485+dev-akshaygupta@users.noreply.github.com>
2025-11-03 01:21:10 -0500

    code clean up

--- static-file-server/main.go
+++ static-file-server/main.go
@@ -6,66 +6,7 @@ import (
 	"net/http"
 )
 
-/*
-	http.Handler - any type that has method - ServeHTTP(w http.ResponseWriter, r *http.Request)
-	ServeHTTP(w http.ResponseWriter, r *http.Request) - interface
-	it can be added to structs or functions
-	struct is like a custom object which translates to object of handler (in leyman terms)
-*/
-// custom handler - HelloHandler
-type GreetingHandler struct {
-	Message string
-}
-
-func (g GreetingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, g.Message)
-	fmt.Fprintln(w, "This is written inside handler")
-}
-
 func main() {
-
-	// Call to custome Handler - HelloHandler
-	http.Handle("/customhandler", GreetingHandler{Message: "This message is passed into the handler while calling it!!"})
-
-	// Custom message - text
-	// w http.ResponseWriter - consider like a notepad -> that browser will read from
-	// "/about" - URL pattern it will respond to
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello this is a custom about page from HandleFunc")
-	})
-
-	// Custom message - HTML tags/page
-	// w http.ResponseWriter - consider like a notepad -> that browser will read from
-	http.HandleFunc("/htmlmsg", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/HTML")
-		fmt.Fprintln(w, "<h3>HTML from HandleFunc<h3>")
-	})
-
-	// Custom message - input from URL (query parameter)
-	http.HandleFunc("/inputlink", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name")
-
-		if name == "" {
-			name = "Guest"
-		}
-
-		fmt.Fprintf(w, "Pleasure to meet you %s\n", name)
-		fmt.Fprintf(w, "We have successfully received request for -  %s\n", name)
-	})
-
-	// Custom message - HTML form (POST request)
-	http.HandleFunc("/thisform", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			r.ParseForm()
-			message := r.FormValue("message")
-			fmt.Fprintf(w, "Decoded message %s\n", message)
-		} else {
-			fmt.Fprintln(w, `<html><body><form method="POST" action="/thisform">
-							<input name="message" placeholder="type something">
-							<input type="submit">
-							</form></html></body>`)
-		}
-	})
 
 	// Directory to store static files
 	staticDir := "./static"
